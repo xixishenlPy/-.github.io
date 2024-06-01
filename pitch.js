60c2aa4cc34bc14217fe4c467b485de7dc9a9257
xixishenlPy <3262763139@qq.com>
2024-06-01 19:36:58 +0800

    Update pitch.js

--- pitch.js
+++ pitch.js
@@ -1,21 +1,48 @@
-// pitch.js
-function createPitchTrace(container, audioFile) {
-  // Code to create pitch trace visualization using D3.js and Pitchfinder
-  const svg = d3.select(container)
-    .append('svg')
-    .attr('class', 'pitch-trace');
+function drawPitch(audioData, container, index) {
+  // 获取 DOM 元素
+  const pitchContainer = d3.select(container)
+    .append('div')
+    .attr('class', 'pitch-item');
 
-  // Load audio file and generate pitch trace
-  d3.json(`/api/pitch?file=${audioFile}`)
-    .then(data => {
-      // Render pitch trace visualization
-      svg.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', (d, i) => i * 10)
-        .attr('cy', d => 200 - d.pitch * 4)
-        .attr('r', 2)
-        .attr('fill', 'red');
-    });
+  // 设置 SVG 画布的大小
+  const width = pitchContainer.node().getBoundingClientRect().width;
+  const height = 200;
+
+  // 创建 SVG 元素
+  const svg = pitchContainer.append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // 缩放函数,将音高数据映射到 SVG 画布上
+  const x = d3.scaleLinear()
+    .range([0, width]);
+  const y = d3.scaleLinear()
+    .range([height, 0]);
+
+  // 计算音高数据
+  const pitchData = getPitchData(audioData);
+
+  // 绘制音高轨迹
+  const line = d3.line()
+    .x((d, i) => x(i / pitchData.length * width))
+    .y(d => y(d));
+
+  svg.append("path")
+    .datum(pitchData)
+    .attr("class", "pitch")
+    .attr("d", line);
+}
+
+function getPitchData(audioData) {
+  // 使用 Pitchfinder 库计算音高数据
+  const pitchDetector = Pitchfinder.YIN();
+  const pitchData = [];
+
+  for (let i = 0; i < audioData.length; i += 512) {
+    const chunk = audioData.slice(i, i + 512);
+    const pitch = pitchDetector(chunk);
+    pitchData.push(pitch || 0);
+  }
+
+  return pitchData;
 }
