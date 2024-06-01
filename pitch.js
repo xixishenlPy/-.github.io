3eb0caa972fa28a48ac56e653af99e650af657bd
xixishenlPy <3262763139@qq.com>
2024-06-01 20:08:25 +0800

    Update pitch.js

--- pitch.js
+++ pitch.js
@@ -13,36 +13,14 @@ function drawPitch(audioData, container, index) {
     .attr("width", width)
     .attr("height", height);
 
-  // 缩放函数,将音高数据映射到 SVG 画布上
-  const x = d3.scaleLinear()
-    .range([0, width]);
-  const y = d3.scaleLinear()
-    .range([height, 0]);
+  // 绘制音高轨迹的占位符（这里用一个圆形代替）
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const radius = 50;
 
-  // 计算音高数据
-  const pitchData = getPitchData(audioData);
-
-  // 绘制音高轨迹
-  const line = d3.line()
-    .x((d, i) => x(i / pitchData.length * width))
-    .y(d => y(d));
-
-  svg.append("path")
-    .datum(pitchData)
-    .attr("class", "pitch")
-    .attr("d", line);
-}
-
-function getPitchData(audioData) {
-  // 使用 Pitchfinder 库计算音高数据
-  const pitchDetector = Pitchfinder.YIN();
-  const pitchData = [];
-
-  for (let i = 0; i < audioData.length; i += 512) {
-    const chunk = audioData.slice(i, i + 512);
-    const pitch = pitchDetector(chunk);
-    pitchData.push(pitch || 0);
-  }
-
-  return pitchData;
+  svg.append("circle")
+    .attr("cx", centerX)
+    .attr("cy", centerY)
+    .attr("r", radius)
+    .attr("fill", "red");
 }
